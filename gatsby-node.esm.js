5ef3c6a738dddee7a1f5302c811e4ec1cb6846d5
Cliff Sanchez <clifford.sanchez@oracle.com>
2020-05-20 21:55:02 -0400

    Added source-pluggin for pulling data via grapql

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -1,52 +1,6 @@
 import {decode as atob, encode as btoa} from 'base-64';
-const { ApolloClient } = require("apollo-client");
-const { InMemoryCache } = require("apollo-cache-inmemory");
-const { split } = require("apollo-link");
-const { HttpLink } = require("apollo-link-http");
-const { WebSocketLink } = require("apollo-link-ws");
-const { getMainDefinition } = require("apollo-utilities");
-const fetch = require("node-fetch");
-const gql = require("graphql-tag");
-const WebSocket = require("ws");
-exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
 
 const axios = require('axios');
-
-const fetchBlogPosts = () => {
-      // fetch raw data from the randomuser api
-      var liferay_url = 'http://localhost:8080/o/headless-delivery/v1.0/sites/20123/blog-postings/';
-      var username = 'bruno';
-      var password = 'bruno';
-      var basicAuth = 'Basic ' + btoa(username + ':' + password);
-
-      const AllBlogPosts = () => axios.get(liferay_url, {}, {
-          headers: { 'Authorization': + basicAuth }
-        }).then(function(response) {
-          console.log('Authenticated');
-        }).catch(function(error) {
-          console.log('Error on Authentication');
-        });
-      
-        return AllBlogPosts;
-}
-
-exports.createPages = async ({ actions: { createPage } }) => {
-
-    // await for results
-    const allBlogs = await fetchBlogPosts();
-    
-     // Create a page that lists all Pokémon.
-     createPage({
-      path: `/`,
-      component: require.resolve(`./src/pages/templates/blog-posts.js`),
-      context: { allBlogs }
-    });
-    
-
-    // map into these results and create nodes
-    console.log('This is a test debug statements');
-    
-}
 
 // constants for your GraphQL Post and Author types
 const POST_NODE_TYPE = `Post`
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,4 +19,4 @@ const IndexPage = () => (
  
 )
 
-export default IndexPage
+export default IndexPage;

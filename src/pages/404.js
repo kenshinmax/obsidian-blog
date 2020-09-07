61c8b953bc85d3e0f7ede6ee1155f6a5359efcc6
Cliff Sanchez <kenshinmax@gmail.com>
2020-09-06 23:27:23 -0400

    Removed page-2

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,27 @@
 import React from "react"
-
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
 
-export default NotFoundPage
+const NotFoundPage = ({ data }) => {
+  return (
+    <div>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+  ) 
+}
+  
+
+export default NotFoundPage;
+
+export const notFoundQuery = graphql `
+  query NotFoundQuery {
+   site {
+    siteMetadata {
+      title
+      description
+    }
+   }
+  }
+`;
